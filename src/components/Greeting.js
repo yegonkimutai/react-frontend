b40e06fecfb1811008082a9995ce1924a5d73f30
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-08-15 13:42:38 +0300

    :sparkles:Add frontend config

---
+++ src/components/Greeting.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGreetings } from "../redux/greetings/greetingSlice";
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const { greetings } = useSelector((state) => state.greetings);
+
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {greetings && <h2>{greetings.greeting}</h2>}
+    </div>
+  );
+};
+
+export default Greeting;
